fb6e367fd6fef08f6215504eddd281186baf46ed
Sina <dalvandsina@yahoo.com>
2021-08-08 03:52:07 +0430

    S3E7-bind-Q

--- app/src/main/java/ir/roocket/sinadalvand/todo/view/SettingsActivity.kt
+++ app/src/main/java/ir/roocket/sinadalvand/todo/view/SettingsActivity.kt
@@ -7,6 +7,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import ir.roocket.sinadalvand.todo.R
 import ir.roocket.sinadalvand.todo.ToDoApplication
 import ir.roocket.sinadalvand.todo.data.local.PrefValuetor
+import ir.roocket.sinadalvand.todo.data.local.Valutor
 import kotlinx.android.synthetic.main.activity_settings.*
 import javax.inject.Inject
 
@@ -14,26 +15,26 @@ import javax.inject.Inject
 class SettingsActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var prefValutor:PrefValuetor
+    lateinit var valuetor:Valutor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        checkCloudSync(prefValutor)
+        checkCloudSync(valuetor)
 
 
         activity_setting_switch.setOnCheckedChangeListener { _, isChecked ->
             lifecycleScope.launchWhenResumed {
-                prefValutor.cloudOn(isChecked)
+                valuetor.cloudOn(isChecked)
             }
         }
     }
 
 
-    private fun checkCloudSync(prefValutor:PrefValuetor){
+    private fun checkCloudSync(valuetor:Valutor){
         lifecycleScope.launchWhenResumed {
-            activity_setting_switch.isChecked = prefValutor.isCloudOn()
+            activity_setting_switch.isChecked = valuetor.isCloudOn()
         }
     }
 
